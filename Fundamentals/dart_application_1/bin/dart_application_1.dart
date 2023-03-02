e080a7acb1cd9bd15381ea37dfa2b6f2335fbd62
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2023-03-02 02:22:32 -0300

    Fundamentals - Day one

--- Fundamentals/dart_application_1/bin/dart_application_1.dart
+++ Fundamentals/dart_application_1/bin/dart_application_1.dart
@@ -1,34 +1,54 @@
-import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
 
-void main(){
+void main() {
   presentation("Enrico", body: (i) {
-    for(int j = 0; j < i; j++){
+    for (int j = 0; j < i; j++) {
       print('Hello for the ${j}th time');
     }
   });
+  String? txt = example(8);
+  print(txt);
+
+  String txt2 = example(8) ?? "Return null!";
+  print(txt2);
 }
 
-void presentation(String name, {
-  String? client,
-  required Function(int) body
-}){
+String? example(int x) {
+  if (x < 7) {
+    return "Hello, world!";
+  }
+}
+
+void presentation(String name, {String? client, required Function(int) body}) {
   print("$name is sending 'Helo!' to you...");
 
   String wrapC = client ?? "No client";
   print("There is a client: ${check(client).toString()}");
 
-  if(check(client)){
+  if (check(client)) {
     print("Welcome, $client!");
   }
 
   body(5);
-
 }
 
-bool check(String? client){
-  if(client != null){
+bool check(String? client) {
+  if (client != null) {
     return true;
   }
 
   return false;
+}
+
+void lists() {
+  List things = ['Enrico', 19, true, 30000.95];
+
+  List<String> names = ["Sundar Pichai", "Tim Cook", "Craig Federighi"];
+
+  print(things.first);
+  print(things[0]);
+
+  print(names.first);
+  print(names[0]);
 }
